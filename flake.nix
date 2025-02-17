9e13afff4725b72a908c2a517b99cf0fb93673ed
DaveVED <dave.w.dennis@gmail.com>
2025-02-17 10:32:03 -0600

    feat(init): add flakes

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Discord bot with Bun and Vercel AI SDK";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+            nodejs_20
+          ];
+          
+          shellHook = ''
+            echo "Discord Bot Development Environment"
+            echo "Run 'bun install' to get started"
+          '';
+        };
+
+        packages.default = pkgs.writeScriptBin "start-bot" ''
+          #!/bin/sh
+          bun run bot.ts
+        '';
+      });
+}
