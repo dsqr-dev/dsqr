fd39c9d38a25ff8db423dfea7e7d42a7a5f3fcee
DaveVED <dave.w.dennis@gmail.com>
2025-03-01 17:47:24 -0600

    feat: the bot can connect, basic stub

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -13,3 +13,20 @@ async function query(apiKey: string) {
 }
 
 export { query }
+
+import { createPerplexity } from "@ai-sdk/perplexity"
+import { generateText } from "ai"
+
+  /*const config = createConfig()
+
+  console.log(config.ai.perplexity.key)
+  const perplexity = createPerplexity({
+    apiKey: config.ai.perplexity.key,
+  })
+
+  const { text } = await generateText({
+    model: perplexity("sonar-pro"),
+    prompt: "What are the latest developments in quantum computing?",
+  })
+
+  console.log(text)*/
